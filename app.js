febf0851e1b8394a4536851d8020a518beef60e3
Christopher Xu <christopher.y.x@gmail.com>
2021-02-15 01:41:17 -0800

    test

--- app.js
+++ app.js
@@ -2,20 +2,22 @@ var express = require('express');
 var app = express();
  
 var server = require('http').createServer(app);
+
+app.use(express.static(process.cwd()));
  
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/client/1L.png');
-    res.sendFile(__dirname + '/client/2L.png');
-    res.sendFile(__dirname + '/client/3L.png');
-    res.sendFile(__dirname + '/client/4L.png');
+    // res.sendFile(__dirname + '/client/1L.png');
+    // res.sendFile(__dirname + '/client/2L.png');
+    // res.sendFile(__dirname + '/client/3L.png');
+    // res.sendFile(__dirname + '/client/4L.png');
 
-    res.sendFile(__dirname + '/client/1R.png');
-    res.sendFile(__dirname + '/client/2R.png');
-    res.sendFile(__dirname + '/client/3R.png');
-    res.sendFile(__dirname + '/client/4R.png');
+    // res.sendFile(__dirname + '/client/1R.png');
+    // res.sendFile(__dirname + '/client/2R.png');
+    // res.sendFile(__dirname + '/client/3R.png');
+    // res.sendFile(__dirname + '/client/4R.png');
 
-    res.sendFile(__dirname + '/client/lalolib.js');
-    res.sendFile(__dirname + '/client/util.js');
+    // res.sendFile(__dirname + '/client/lalolib.js');
+    // res.sendFile(__dirname + '/client/util.js');
     res.sendFile(__dirname + '/client/sim.html');
 });
 
